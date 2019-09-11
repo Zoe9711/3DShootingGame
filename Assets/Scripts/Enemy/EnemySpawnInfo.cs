a0d6eb07ea9c10bdd4d99accf822b5d4e55b994a
Zoe Zhang <Hurricane@Hurricane.local>
2019-09-10 23:57:26 -0700

    completed

---
+++ Assets/Scripts/Enemy/EnemySpawnInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySpawnInfo
+{
+    #region
+    [SerializeField]
+    [Tooltip("the name of this enemy")]
+    private string m_Name;
+    public string EnemyName
+    {
+        get
+        {
+            return m_Name;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("the prefab of this enemy that will be spawned")]
+    private GameObject m_EnemyGO;
+    public GameObject EnemyGO
+    {
+        get
+        {
+            return m_EnemyGO;
+        }
+    }
+
+
+    [SerializeField]
+    [Tooltip("the number of seconds before the next enemy is spawned")]
+    private float m_TimeToNextSpawn;
+    public float TimeToNextSpawn
+    {
+        get
+        {
+            return m_TimeToNextSpawn;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("the number of enemies to spawn. if set to 0, it will spawn endlessly")]
+    private int m_NumberToSpawn;
+    public int NumberToSpawn
+    {
+        get
+        {
+            return m_NumberToSpawn;
+        }
+    }
+
+
+    #endregion
+}
+
---
+++ Assets/Scripts/Player/PlayerFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("the player to follow")]
+    private Transform m_PlayerTransform;
+
+    [SerializeField]
+    [Tooltip("the offset from the player's origin to the camera")]
+    private Vector3 m_Offset;
+
+    [SerializeField]
+    [Tooltip("how fast the player can rotate the camera to the left and right")]
+    private float m_RotationSpeed = 10;
+
+    #endregion
+
+    #region Main Updates
+    private void LateUpdate()
+    {
+        Vector3 newPos = m_PlayerTransform.position + m_Offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, 1);
+
+        float rotationxAmount = m_RotationSpeed * Input.GetAxis("Mouse X");
+        float rotationyAmount = m_RotationSpeed * Input.GetAxis("Mouse Y");
+
+        transform.RotateAround(m_PlayerTransform.position, Vector3.up, rotationxAmount + rotationyAmount);
+
+        m_Offset = transform.position - m_PlayerTransform.position;
+    } 
+
+
+    #endregion
+}
